415310515f4d3f7d1aaf32e1f1baee6d9da79749
irohanrajput <itusharsingh27@gmail.com>
2024-06-24 09:39:51 +0530

    switching os

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -1,17 +1,27 @@
+import React, { useState } from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
 import Lottie from 'react-lottie';
 import animationData from '../../public/job_lottie.json';
 
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
+
 function SignUp() {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+
+  
+
   return (
     <>
       <Navbar />
@@ -25,23 +35,30 @@ function SignUp() {
               alt=""
             />
             <div className="text-4xl w-3/4 flex-col justify-center text-center m-4">
-              <form action="" method="post">
+              <form >
                 <div className="text-3xl text-center flex flex-col items-center bg-[#52515f] p-4 m-4 rounded-3xl shadow-default ">
                   <h6 className="mb-4">Create an account</h6>
+
+                  <label htmlFor="username" className="font-mono text-2xl">
+                    select an username
+                  </label>
+                  <input type="text" id='userId' value={username} onChange={(e)=> setUsername(e.target.value)} placeholder=''  />
+
                   <label htmlFor="email" className="font-mono text-2xl">
                     your email
                   </label>
-                  <input id="email" className="" type="text" />
+                  <input id="email" className="" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
 
                   <label htmlFor="password" className="font-mono text-2xl">
                     password
                   </label>
-                  <input id="password" type="password" />
+                  <input id="password" type="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
 
-                  <label htmlFor="password" className="font-mono text-2xl">
+                  <label htmlFor="confirmPassword" className="font-mono text-2xl">
                     confirm password
                   </label>
-                  <input id="password" type="password" />
+                  <input id="confirmPassword" type="password" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)} />
+
                   <button className="">Sign up</button>
                 </div>
               </form>
